f33a6d3d6f2fd40466af6326a09684c38ae64c83
aisuluu777 <a30139706@gmail.com>
2025-02-01 16:37:29 +0600

    modified hw8

--- handlers/group_ban.py
+++ handlers/group_ban.py
@@ -10,10 +10,10 @@ ban_router.message.filter(F.chat.type != 'private')
 
 @ban_router.message(F.text)
 async def ban_handler(message: types.Message):
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
+    if message:
+        user_id = message.from_user.id
         for word in ban_words:
-            if word in message.reply_to_message.text.lower():
+            if word in message.text.lower():
                 await message.chat.ban(user_id)
 
 
